ba8c2bc67afd494f3fee4e57ac357f22b9be4c1d
VivienWeber <vivien_weber@gmx.net>
2024-06-20 14:33:35 +0200

    add comments for better understanding

--- oglTemplate/oglViewer.py
+++ oglTemplate/oglViewer.py
@@ -23,7 +23,7 @@
  *          OpenGL 3.2 core profile context and animate a colored triangle.
  ****
 """
-
+import sys
 import glfw
 import numpy as np
 
@@ -43,21 +43,22 @@ class Scene:
 
     def __init__(self, width, height, scenetitle="Hello Triangle"):
         self.scenetitle         = scenetitle
-        self.width              = width
-        self.height             = height
-        self.angle              = 0
-        self.angle_increment    = 1
-        self.animate            = False
+        self.width              = width     # Breite des Fensters
+        self.height             = height    # Höhe des Fensters
+        self.angle              = 0         # Winkel der Rotation
+        self.angle_increment    = 1         # Inkrement für die Rotation
+        self.animate            = False     # steuert die Animation
 
 
     def init_GL(self):
         # setup buffer (vertices, colors, normals, ...)
-        self.gen_buffers()
+        self.gen_buffers()                  # erzeugt und initialisiert die Pufferobjekte
 
         # setup shader
         glBindVertexArray(self.vertex_array)
         vertex_shader       = open("shader.vert","r").read()
         fragment_shader     = open("shader.frag","r").read()
+        # compileShader() liest und kompiliert die Shader und erstellt ein Shader-Programm
         vertex_prog         = compileShader(vertex_shader, GL_VERTEX_SHADER)
         frag_prog           = compileShader(fragment_shader, GL_FRAGMENT_SHADER)
         self.shader_program = compileProgram(vertex_prog, frag_prog)
@@ -65,7 +66,7 @@ class Scene:
         # unbind vertex array to bind it again in method draw
         glBindVertexArray(0)
 
- 
+
     def gen_buffers(self):
         # TODO: 
         # 1. Load geometry from file and calc normals if not available
@@ -76,17 +77,18 @@ class Scene:
         glBindVertexArray(self.vertex_array)
 
         # generate and fill buffer with vertex positions (attribute 0)
-        positions = np.array([  0.0,  0.58,  0.0, # 0. vertex
-                               -0.5, -0.29,  0.0, # 1. vertex
-                                0.5, -0.29,  0.0, # 2. vertex
-                                0.0,  0.00, -0.58 # 3. vertex
-                                ], dtype=np.float32)
+        positions = np.array([
+                                    0.0,  0.58,  0.0,   # 0. vertex
+                                    -0.5, -0.29,  0.0,  # 1. vertex
+                                    0.5, -0.29,  0.0,   # 2. vertex
+                                    0.0,  0.00, -0.58   # 3. vertex
+                                    ], dtype=np.float32)
         pos_buffer = glGenBuffers(1)
         glBindBuffer(GL_ARRAY_BUFFER, pos_buffer)
         glBufferData(GL_ARRAY_BUFFER, positions.nbytes, positions, GL_STATIC_DRAW)
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, None)
         glEnableVertexAttribArray(0)
- 
+
         # generate and fill buffer with vertex colors (attribute 1)
         colors = np.array([ 1.0, 0.0, 0.0, # 0. color
                             0.0, 1.0, 0.0, # 1. color
@@ -100,11 +102,12 @@ class Scene:
         glEnableVertexAttribArray(1)
 
         # generate index buffer (for triangle strip)
-        self.indices = np.array([0, 1, 2, 3, 0, 1], dtype=np.int32)        
+        # Indizes: definiert die Reihenfolge, in der die Vertices gezeichnet werden
+        self.indices = np.array([0, 1, 2, 3, 0, 1], dtype=np.int32)
         ind_buffer_object = glGenBuffers(1)
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ind_buffer_object)
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, self.indices.nbytes, self.indices, GL_STATIC_DRAW)
-        
+
         # unbind buffers to bind again in draw()
         glBindBuffer(GL_ARRAY_BUFFER, 0)
         glBindVertexArray(0)
@@ -133,16 +136,16 @@ class Scene:
         if self.animate:
             # increment rotation angle in each frame
             self.angle += self.angle_increment
-    
+
         # setup matrices
         projection = perspective(45.0, self.width/self.height, 1.0, 5.0)
-        view       = look_at(0,0,2, 0,0,0, 0,1,0)
+        view       = look_at(0, 0, 2, 0, 0, 0, 0, 1, 0)
         model      = rotate_y(self.angle)
         mvp_matrix = projection @ view @ model
 
         # enable shader & set uniforms
         glUseProgram(self.shader_program)
-        
+
         # determine location of uniform variable varName
         varLocation = glGetUniformLocation(self.shader_program, 'modelview_projection_matrix')
         # pass value to shader
@@ -150,12 +153,12 @@ class Scene:
 
         # enable vertex array & draw triangle(s)
         glBindVertexArray(self.vertex_array)
-        glDrawElements(GL_TRIANGLE_STRIP, self.indices.nbytes//4, GL_UNSIGNED_INT, None)
+        glDrawElements(GL_TRIANGLE_STRIP, self.indices.nbytes//4, GL_UNSIGNED_INT, None)    # zeichnet das Tetraeder
 
         # unbind the shader and vertex array state
         glUseProgram(0)
         glBindVertexArray(0)
-        
+
 
 
 class RenderWindow:
@@ -194,7 +197,7 @@ class RenderWindow:
         glfw.set_window_size_callback(self.window, self.on_size)
 
         # create scene
-        self.scene = scene  
+        self.scene = scene
         if not self.scene:
             glfw.terminate()
             sys.exit(EXIT_FAILURE)
@@ -213,7 +216,7 @@ class RenderWindow:
         # print('Renderer     : %s' % glGetString(GL_RENDERER))
 
         # set background color to black
-        glClearColor(0, 0, 0, 0)     
+        glClearColor(0, 0, 0, 0)
 
         # Enable depthtest
         glEnable(GL_DEPTH_TEST)
@@ -265,7 +268,7 @@ class RenderWindow:
 
             # call the rendering function
             self.scene.draw()
-            
+
             # swap front and back buffer
             glfw.swap_buffers(self.window)
 
